edf7e34806b8ba723a04ddb27c62277eab996f59
Andrea Grande <andrea.grande.ext@infocert.it>
2020-04-08 12:59:13 +0200

    Kubernetes run script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+export imagename=ml-microservice-api
+export appname=ml-predictions-app
+export dockerpath=ookiisan/$imagename
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run $appname --generator=run-pod/v1 --image=$dockerpath --port=80 --labels app=$appname
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
+# Wait for pod readiness
+kubectl wait --for=condition=Ready pod -l app=$appname --timeout=180s
 # Forward the container port to a host
-
+kubectl port-forward $appname 8000:80
